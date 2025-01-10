2541f82b590bd32d0bf62e984f95c5b94c782f96
rt33 <rt33@lastcycle.com>
2025-01-10 12:22:09 +0900

    りふぁくた不要なメモリ計算削除

--- main.go
+++ main.go
@@ -1,107 +1,154 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"math/cmplx"
 	"os"
-	"runtime"
 	"sync"
 )
 
-type Parameters struct {
-	XMin, YMin, XMax, YMax float64
-	Width, Height          int
-	SubPixelsSamples       int
-	MaxIterations          int
-	Contrast               int
+type Generator struct {
+	params Parameters
 }
+
+type Parameters struct {
+	ViewPort struct {
+		XMin, YMin float64
+		XMax, YMax float64
+	}
+	Size struct {
+		Width  int
+		Height int
+	}
+	RenderOpts struct {
+		SubPixelSamples int
+		MaxIterations   int
+		Contrast        int
+	}
+}
+
+// 不正なパラメータが指定された場合のエラー
+var ErrInvalidParameters = errors.New("invalid parameters")
 
 func NewDefaultParameters() Parameters {
-	return Parameters{
-		XMin: -2, YMin: -2,
-		XMax: 2, YMax: 2,
-		Width: 1024, Height: 1024,
-		SubPixelsSamples: 4,
-		MaxIterations:    200,
-		Contrast:         15,
+	p := Parameters{}
+	p.ViewPort.XMin = -2
+	p.ViewPort.YMin = -2
+	p.ViewPort.XMax = 2
+	p.ViewPort.YMax = 2
+	p.Size.Width = 1024
+	p.Size.Height = 1024
+	p.RenderOpts.SubPixelSamples = 4
+	p.RenderOpts.MaxIterations = 200
+	p.RenderOpts.Contrast = 15
+	return p
+}
+
+func NewGenerator(params Parameters) (*Generator, error) {
+	if err := validateParameters(params); err != nil {
+		return nil, fmt.Errorf("invalid parameters: %w", err)
 	}
+	return &Generator{params: params}, nil
 }
 
-// Point はサンプリングポイント
-type Point struct {
-	X, Y float64
-}
-
-func main() {
-	params := NewDefaultParameters()
-	img := GenerateMandelbrot(params)
-
-	if err := saveImage(img, "mandelbrot.png"); err != nil {
-		panic(err)
+func validateParameters(p Parameters) error {
+	if p.ViewPort.XMax <= p.ViewPort.XMin || p.ViewPort.YMax <= p.ViewPort.YMin {
+		return fmt.Errorf("%w: invalid viewport range", ErrInvalidParameters)
 	}
+	if p.Size.Width <= 0 || p.Size.Height <= 0 {
+		return fmt.Errorf("%w: invalid image size", ErrInvalidParameters)
+	}
+	if p.RenderOpts.SubPixelSamples <= 0 {
+		return fmt.Errorf("%w: invalid subpixel samples", ErrInvalidParameters)
+	}
+	return nil
 }
 
-func GenerateMandelbrot(params Parameters) *image.RGBA {
-	img := image.NewRGBA(image.Rect(0, 0, params.Width, params.Height))
-	samplingWidth := 0.5 / float64(params.Width) * (params.XMax - params.XMin)
-	samplingHeight := 0.5 / float64(params.Height) * (params.YMax - params.YMin)
+// point はサンプリングポイントを表す
+type point struct {
+	x, y float64
+}
 
-	numWorkers := runtime.NumCPU()
-	runtime.GOMAXPROCS(numWorkers)
+func (g *Generator) Generate(ctx context.Context) (*image.RGBA, error) {
+	img := image.NewRGBA(image.Rect(0, 0, g.params.Size.Width, g.params.Size.Height))
+
+	samplingWidth := 0.5 / float64(g.params.Size.Width) * (g.params.ViewPort.XMax - g.params.ViewPort.XMin)
+	samplingHeight := 0.5 / float64(g.params.Size.Height) * (g.params.ViewPort.YMax - g.params.ViewPort.YMin)
+
 	var wg sync.WaitGroup
-	wg.Add(params.Height)
+	errChan := make(chan error, g.params.Size.Height)
 
-	for py := 0; py < params.Height; py++ {
+	// 各行を並列処理
+	for py := 0; py < g.params.Size.Height; py++ {
+		wg.Add(1)
 		go func(py int) {
 			defer wg.Done()
-			processRow(py, img, params, samplingWidth, samplingHeight)
+			if err := g.processRow(ctx, py, img, samplingWidth, samplingHeight); err != nil {
+				errChan <- err
+			}
 		}(py)
 	}
 
 	wg.Wait()
-	return img
-}
+	close(errChan)
 
-// Pixel1行分の計算
-func processRow(py int, img *image.RGBA, params Parameters, samplingWidth, samplingHeight float64) {
-	y := float64(py)/float64(params.Height)*(params.YMax-params.YMin) + params.YMin
-
-	for px := 0; px < params.Width; px++ {
-		x := float64(px)/float64(params.Width)*(params.XMax-params.XMin) + params.XMin
-		samples := getSamples(x, y, samplingWidth, samplingHeight, params)
-		avgColor := averageColors(samples)
-		img.Set(px, py, avgColor)
+	for err := range errChan {
+		if err != nil {
+			return nil, fmt.Errorf("error processing row: %w", err)
+		}
 	}
+
+	return img, nil
 }
 
-// スーパーサンプリング用のカラーサンプル
-func getSamples(x, y, samplingWidth, samplingHeight float64, params Parameters) []color.Color {
-	points := []Point{
+// 1行分のピクセルを処理する
+func (g *Generator) processRow(ctx context.Context, py int, img *image.RGBA, samplingWidth, samplingHeight float64) error {
+	y := float64(py)/float64(g.params.Size.Height)*(g.params.ViewPort.YMax-g.params.ViewPort.YMin) + g.params.ViewPort.YMin
+
+	for px := 0; px < g.params.Size.Width; px++ {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			x := float64(px)/float64(g.params.Size.Width)*(g.params.ViewPort.XMax-g.params.ViewPort.XMin) + g.params.ViewPort.XMin
+			samples := g.getSamples(x, y, samplingWidth, samplingHeight)
+			avgColor := averageColors(samples)
+			img.Set(px, py, avgColor)
+		}
+	}
+	return nil
+}
+
+// スーパーサンプリング用のカラーサンプルを取得する
+func (g *Generator) getSamples(x, y, samplingWidth, samplingHeight float64) []color.Color {
+	points := []point{
 		{x, y},
 		{x + samplingWidth, y},
 		{x + samplingWidth, y + samplingHeight},
 		{x, y + samplingHeight},
 	}
 
-	samples := make([]color.Color, len(points))
-	for i, p := range points {
-		samples[i] = mandelbrot(complex(p.X, p.Y), params)
+	samples := make([]color.Color, 0, len(points))
+	for _, p := range points {
+		samples = append(samples, g.mandelbrot(complex(p.x, p.y)))
 	}
 	return samples
 }
 
-// 色の計算
-func mandelbrot(z complex128, params Parameters) color.Color {
+func (g *Generator) mandelbrot(z complex128) color.Color {
 	var v complex128
-	for n := uint8(0); n < uint8(params.MaxIterations); n++ {
+	for n := uint8(0); n < uint8(g.params.RenderOpts.MaxIterations); n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			return color.RGBA{
-				R: 64 - uint8(params.Contrast)*n,
-				G: 80 - uint8(params.Contrast)*n%128,
-				B: 240 + uint8(params.Contrast)*n%64,
+				R: 64 - uint8(g.params.RenderOpts.Contrast)*n,
+				G: 80 - uint8(g.params.RenderOpts.Contrast)*n%128,
+				B: 240 + uint8(g.params.RenderOpts.Contrast)*n%64,
 				A: 255,
 			}
 		}
@@ -109,8 +156,12 @@ func mandelbrot(z complex128, params Parameters) color.Color {
 	return color.Black
 }
 
-// 複数のサンプルから平均色を計算
+// 複数のサンプルから平均色を計算する
 func averageColors(colors []color.Color) color.Color {
+	if len(colors) == 0 {
+		return color.Black
+	}
+
 	var r, g, b, a uint32
 	for _, c := range colors {
 		cr, cg, cb, ca := c.RGBA()
@@ -119,6 +170,7 @@ func averageColors(colors []color.Color) color.Color {
 		b += cb
 		a += ca
 	}
+
 	n := uint32(len(colors))
 	return color.RGBA{
 		R: uint8(r / n >> 8),
@@ -128,11 +180,33 @@ func averageColors(colors []color.Color) color.Color {
 	}
 }
 
-func saveImage(img *image.RGBA, filename string) error {
+func SaveImage(img *image.RGBA, filename string) error {
 	f, err := os.Create(filename)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create file: %w", err)
 	}
 	defer f.Close()
-	return png.Encode(f, img)
+
+	if err := png.Encode(f, img); err != nil {
+		return fmt.Errorf("failed to encode image: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	params := NewDefaultParameters()
+	generator, err := NewGenerator(params)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	img, err := generator.Generate(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := SaveImage(img, "mandelbrot.png"); err != nil {
+		panic(err)
+	}
 }
